8db313227214c0bd9e87e441896ebf92a8e349c0
s-arslaan <shaikharslaan1234@gmail.com>
2022-06-30 16:56:14 +0530

    design

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -2,7 +2,7 @@
 <footer class="py-4 bg-light mt-auto">
   <div class="container-fluid px-4">
     <div class="d-flex align-items-center justify-content-between small">
-      <div class="text-muted">Copyright &copy; Your Website 2022</div>
+      <div class="text-muted">Copyright &copy; Shama Education <?= date("Y") ?></div>
       <div>
         <a href="#">Privacy Policy</a>
         &middot;
@@ -16,8 +16,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="<?= base_url() ?>/public/assets/js/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-<script src="assets/demo/chart-area-demo.js"></script>
-<script src="assets/demo/chart-bar-demo.js"></script>
+<!-- <script src="assets/demo/chart-area-demo.js"></script>
+<script src="assets/demo/chart-bar-demo.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
 <script src="<?= base_url() ?>/public/assets/js/datatables-simple-demo.js"></script>
 </body>
