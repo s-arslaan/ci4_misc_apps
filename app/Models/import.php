85f3f4df6847f8d969508a16f24544484ccaee2d
s-arslaan <shaikharslaan1234@gmail.com>
2022-08-21 21:23:30 +0530

    public folder

--- app/Models/import.php
+++ app/Models/import.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-require 'vendor/autoload.php';
+require ROOTPATH.'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
