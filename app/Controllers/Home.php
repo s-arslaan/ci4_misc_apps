514bdb312af2b48bff3d4f2f0d227647bbf2ee99
s-arslaan <shaikharslaan1234@gmail.com>
2022-08-23 20:17:04 +0530

    remap

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ use CodeIgniter\Exceptions\PageNotFoundException;
 use App\Models\AttendanceModel;
 use App\Models\Users;
 use App\Models\HomeModel;
+use ReflectionMethod;
 
 /**
  * @property IncomingRequest $request 
@@ -126,15 +127,35 @@ class Home extends BaseController
         return view('login_activity', $data);
     }
 
-    public function _remap($method, $params = array())
+    // remap function for checking if method exists with accepted parameter
+    public function _remap($method, $param0 = null, $param1 = null, $param2 = null)
     {
         if (method_exists($this, $method))
         {
-            return call_user_func_array(array($this, $method), $params);
+            if(isset($param2))
+                $temp = $param0.','.$param1.','.$param2;
+            else if(isset($param1))
+                $temp = $param0.','.$param1;
+            else if(isset($param0))
+                $temp = $param0;
+            else
+                $temp = '';
+
+            $params = [];
+            if($temp !== '')
+                $params = explode(',',$temp);
+            // echo '<pre>';print_r($method);echo '<br>';print_r($params);exit;
+
+            $existing = new ReflectionMethod($this,$method);
+            $existing_params = $existing->getParameters();
+
+            if(count($existing_params) == count($params))
+                return call_user_func_array(array($this, $method), $params);
+            else
+                throw PageNotFoundException::forPageNotFound();
         }
         else {
             throw PageNotFoundException::forPageNotFound();
         }
-        
     }
 }
