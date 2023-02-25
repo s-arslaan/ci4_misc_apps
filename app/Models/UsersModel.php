446196993bcd4c770294b8b22672c1f81de733ac
s-arslaan <shaikharslaan1234@gmail.com>
2023-02-25 08:47:49 +0530

    alerts in desc

--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -16,7 +16,7 @@ class UsersModel extends Model
     
     public function getAlerts()
     {
-        $query = $this->db->query("select ba.alert_id, u.name, u.email, u.gToken, ba.timestamp, ba.latitude, ba.longitude, ba.address, ba.city_name, ba.isRescued, ba.remarks from beach_alerts ba LEFT JOIN beach_users u ON(u.gToken = ba.gToken)");
+        $query = $this->db->query("select ba.alert_id, u.name, u.email, u.gToken, ba.timestamp, ba.latitude, ba.longitude, ba.address, ba.city_name, ba.isRescued, ba.remarks from beach_alerts ba LEFT JOIN beach_users u ON(u.gToken = ba.gToken) ORDER BY ba.timestamp DESC");
         $res = $query->getResultArray();
         return $res;
     }
